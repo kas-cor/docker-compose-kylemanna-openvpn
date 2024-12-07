d9dd0b42098e518318ec358b4906874ae5b64e2e
kas-cor <kascorp@gmail.com>
2024-12-07 10:01:17 +0000

    renamed:    .env -> .env.sample new file:   .gitignore modified:   00_install-OpenVPN-Server.sh modified:   docker-compose.yml

--- 00_install-OpenVPN-Server.sh
+++ 00_install-OpenVPN-Server.sh
@@ -2,10 +2,10 @@
 # Check if .env file exists
 if [ -e .env ]; then
     source .env
-else 
+else
     echo "Please set up your .env file before starting your enviornment."
     exit 1
 fi
 
-docker-compose run --rm server ovpn_genconfig -u $PROTO://$HOSTNAME
+docker-compose run --rm server ovpn_genconfig -u $PROTO://$HOSTNAME:$PUBLIC_PORT
 docker-compose run --rm server ovpn_initpki
